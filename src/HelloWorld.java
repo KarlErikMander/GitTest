00602d993b1d3d8bb4bc6401aee145afc23425ef
Karl Erik Mander <mander.karl@gmail.com>
2021-02-24 16:13:50 +0200

    OH NO CONFLICT

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Reject humanity, return to monke");
+        System.out.println("GO BACK TO MONKE! OH NO MONKE CONFLICT ");
     }
 }
